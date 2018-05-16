44cc3469adfcfb149fc847451a003c8aa6f7f5b9
buivanxung <xungbv.uit@gmail.com>
2018-05-16 09:06:08 +0700

    develop

---
+++ server.js
@@ -0,0 +1,96 @@
+
+// Setup basic express server
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.engine('html', require('ejs').renderFile);
+app.use(require('stylus').middleware({ src: __dirname + '/app' }));
+app.use(express.static(__dirname + '/app'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/');
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/data";
+
+function delay(ms) {
+   ms += new Date().getTime();
+   while (new Date() < ms){}
+}
+function getNextSequenceValue(sequenceName){
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("data");
+        var sequenceDocument = dbo.counters.findAndModify({
+        query:{_id: sequenceName },
+        update: {$inc:{sequence_value:1}},
+        new:true
+          });
+      return sequenceDocument.sequence_value;
+    });
+  }
+
+function InsertToDatabase(data, gio, ngay) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log("Error connect database");
+    }
+    var dbo = db.db("data");
+    var myobj = { DuLieu: data, ThoiGian: gio, Ngay: ngay };
+    dbo.collection("DuLieu").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+}
+
+// Routing
+app.get('/',function(req,res){
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("data");
+    dbo.collection("DuLieu").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.render('web', {collection:result} );
+      db.close();
+    });
+  });
+})
+
+io.on('connection', function (socket) {
+  console.log("New connection");
+  socket.on('request', function(data) {
+    console.log("Recived!");
+    if (data == 12) {
+      socket.emit('event', 12);
+      console.log("Event sending!");
+    }
+  });
+  socket.on('message_server',function(data){
+    socket.broadcast.emit('message_client', data);
+    console.log(data);
+  });
+  socket.on('control_status', function(data) {
+    socket.broadcast.emit('client_control', data);
+  });
+  socket.on('web_control_status', function(data) {
+    socket.broadcast.emit('client_control_status', data);
+  });
+
+  socket.on('feedback', function(data) {
+    console.log(data);
+    var date = new Date();
+    InsertToDatabase(data, date.toLocaleTimeString(),date.toLocaleDateString());
+    console.log("Success");
+    socket.broadcast.emit('message', data);
+  });
+  socket.on('status', function(data) {
+    socket.broadcast.emit('status_client', data);
+    console.log(data);
+  });
+});
+
+http.listen(5000, function () {
+  console.log("Server running with port 5000");
+});
