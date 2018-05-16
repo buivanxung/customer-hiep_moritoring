3320139f50e42f5c89c3000f876124cc4220d788
Bùi Văn Xứng UIT <xungbv.uit@gmail.com>
2018-05-16 20:15:42 +0700

    fix data

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ io.on('connection', function (socket) {
   });
   portR.on('data', function (data) {
     console.log('Data sent R:', "" +data);
-    socket.broadcast.emit('feedback', data);
+    socket.broadcast.emit('feedback', data + "R");
   });
   portR.on('readable', function () {
       console.log('Data:', port.read());
